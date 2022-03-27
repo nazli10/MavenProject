d30da472a5c634c6a11ba12a8313885bb568a7c0
nazli10 <99680320+nazli10@users.noreply.github.com>
2022-03-27 04:13:12 +0200

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
 
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("Local Repo is already published to Github");
+        System.out.println("Doing more work");
     }
 }
